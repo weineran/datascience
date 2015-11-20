ced95ef2cb9332da6aeb99ae2e626ee398d160ba
Andrew <weiner.andrew.j@gmail.com>
2015-11-20 15:27:22 -0600

    a test that shows markov chain in action

--- mc-test.py
+++ mc-test.py
@@ -3,7 +3,7 @@
 from pymarkovchain import MarkovChain
 # Create an instance of the markov chain. By default, it uses MarkovChain.py's location to
 # store and load its database files to. You probably want to give it another location, like so:
-mc = MarkovChain("/home/awp066/markovchain")
+mc = MarkovChain("C:/Users/Andrew/OneDrive/Documents/Northwestern/Courses/495-Data-Science/Final Project")
 # To generate the markov chain's language model, in case it's not present
 mc.generateDatabase("It is nice to meet you.  I would like to meet your friend.")
 # To let the markov chain generate some text, execute
